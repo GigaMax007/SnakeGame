33abbc9407f012aeb128ade8a155655872aebf03
Maksim Kishinskiy <m.kishinskiy@gmail.com>
2020-04-12 19:01:04 +0500

    Создана программа по лекции Ирины Галкиной

---
+++ src/MainWindow.java
@@ -0,0 +1,22 @@
+/**
+ * Создано Максимом Кишинским по лекциям Ирины Галкиной!
+ * Спасибо ей за это)))
+ */
+
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+    public MainWindow(){
+        setTitle("Змейка");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(370, 390);
+        setLocation(400,400);
+        add(new GameField());
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MainWindow mw = new MainWindow();
+    }
+
+}
